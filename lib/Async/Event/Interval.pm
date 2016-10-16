8dcd661bca56a61a52b552145dd32531f22c2005
Steve Bertrand <steveb@cpan.org>
2016-10-16 09:24:23 -0600

    removed appveyor button in pod

--- lib/Async/Event/Interval.pm
+++ lib/Async/Event/Interval.pm
@@ -75,7 +75,6 @@ Async::Event::Interval - Extremely simple timed asynchronous events
 
 =for html
 <a href="http://travis-ci.org/stevieb9/async-event-interval"><img src="https://secure.travis-ci.org/stevieb9/async-event-interval.png"/></a>
-<a href="https://ci.appveyor.com/project/stevieb9/async-event-interval"><img src="https://ci.appveyor.com/api/projects/status/br01o72b3if3plsw/branch/master?svg=true"/></a>
 <a href='https://coveralls.io/github/stevieb9/async-event-interval?branch=master'><img src='https://coveralls.io/repos/stevieb9/async-event-interval/badge.svg?branch=master&service=github' alt='Coverage Status' /></a>
 
 
