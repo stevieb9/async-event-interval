fa59848fb05a035a38b2a2256a6e81fa5137d8f1
Steve Bertrand <steveb@cpan.org>
2022-03-03 19:20:30 -0800

    Indented shared data example in POD

--- lib/Async/Event/Interval.pm
+++ lib/Async/Event/Interval.pm
@@ -663,54 +663,54 @@ and update the same hash.
 The very last line removes the shared memory segments. You can remove this line
 if you want to reuse the same data later, or access it from other scripts.
 
-use Async::Event::Interval;
-use IPC::Shareable;
+    use Async::Event::Interval;
+    use IPC::Shareable;
 
-tie my %shared_data, 'IPC::Shareable', {
-    key         => '123456789',
-    create      => 1,
-    exclusive   => 0,
-    destroy     => 1
-};
-
-$shared_data{$$}++;
-
-my $event_one = Async::Event::Interval->new(0.2, \&update);
-my $event_two = Async::Event::Interval->new(1, \&update);
-
-$event_one->start;
-$event_two->start;
-
-sleep 10;
-
-$event_one->stop;
-$event_two->stop;
-
-for my $pid (keys %shared_data) {
-    printf("Process ID %d executed %d times\n", $pid, $shared_data{$pid});
-}
-
-for my $event ($event_one, $event_two) {
-    printf(
-        "Event ID %d with PID %d ran %d times, with %d errors and an interval" .
-        " of %.2f seconds\n",
-        $event->id,
-        $event->pid,
-        $event->runs,
-        $event->errors,
-        $event->interval
-    );
-}
-
-(tied %shared_data)->remove;
-
-sub update {
-    # Because each event runs in its own process, $$ will be set to the
-    # process ID of the calling event, even though they both call this
-    # same function
+    tie my %shared_data, 'IPC::Shareable', {
+        key         => '123456789',
+        create      => 1,
+        exclusive   => 0,
+        destroy     => 1
+    };
 
     $shared_data{$$}++;
-}
+
+    my $event_one = Async::Event::Interval->new(0.2, \&update);
+    my $event_two = Async::Event::Interval->new(1, \&update);
+
+    $event_one->start;
+    $event_two->start;
+
+    sleep 10;
+
+    $event_one->stop;
+    $event_two->stop;
+
+    for my $pid (keys %shared_data) {
+        printf("Process ID %d executed %d times\n", $pid, $shared_data{$pid});
+    }
+
+    for my $event ($event_one, $event_two) {
+        printf(
+            "Event ID %d with PID %d ran %d times, with %d errors and an interval" .
+            " of %.2f seconds\n",
+            $event->id,
+            $event->pid,
+            $event->runs,
+            $event->errors,
+            $event->interval
+        );
+    }
+
+    (tied %shared_data)->remove;
+
+    sub update {
+        # Because each event runs in its own process, $$ will be set to the
+        # process ID of the calling event, even though they both call this
+        # same function
+
+        $shared_data{$$}++;
+    }
 
 =head1 AUTHOR
 
